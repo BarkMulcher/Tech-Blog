828b9f4359d6620168cefed290a4c84d4088497c
BarkMulcher <lucas.c.haskell@gmail.com>
2022-10-13 19:09:14 -0400

    delete extra code

--- controllers/commentRoutes.js
+++ controllers/commentRoutes.js
@@ -18,20 +18,3 @@ router.post('/', async (req, res) => {
 
 module.exports = router;
 
-// try {
-//     const user_id = req.session.user_id;
-//     const { blog_id, content } = req.body;
-
-//     if (!user_id || !blog_id || !content) {
-//         res.json({ message: 'No comment'});
-//     }
-
-//     const newComment = await Comment.create({
-//         content,
-//         blog_id,
-//         user_id
-//     })
-//     res.status(200).json(newComment)
-// } catch (err) {
-//     res.status(500).json(err)
-// }
